a571764ff637038fe6e0dfdae27a08b77e788352
Lucas <lucaspedrazoli@gmail.com>
2020-11-06 18:04:58 -0300

    add ignore elements example

--- RxSwiftPlayground.playground/Contents.swift
+++ RxSwiftPlayground.playground/Contents.swift
@@ -1,2 +1,24 @@
 import Foundation
 import RxSwift
+
+example(of: "Ignore elements") {
+  let strikes = PublishSubject<String>()
+  let disposeBag = DisposeBag()
+
+  strikes
+    .ignoreElements()
+    .subscribe { _ in
+      print("You're out!")
+    }
+    .disposed(by: disposeBag)
+
+  strikes.onNext("X")
+  strikes.onNext("X")
+  strikes.onNext("X")
+  strikes.onCompleted()
+}
+
+example(of: "Element at") {
+  
+}
+
