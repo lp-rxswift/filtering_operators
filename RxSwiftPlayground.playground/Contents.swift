e865d2ede7accff026a033d04afe4b8c2dc2bcb3
Lucas <lucaspedrazoli@gmail.com>
2020-11-07 19:30:14 -0300

    add elementAt and filter operator examples

--- RxSwiftPlayground.playground/Contents.swift
+++ RxSwiftPlayground.playground/Contents.swift
@@ -19,6 +19,24 @@ example(of: "Ignore elements") {
 }
 
 example(of: "Element at") {
-  
+  let strikes = PublishSubject<String>()
+  let disposeBag = DisposeBag()
+  strikes
+    .elementAt(2)
+    .subscribe(onNext: { _ in
+      print("You're out!")
+    })
+    .disposed(by: disposeBag)
+
+  strikes.onNext("X")
+  strikes.onNext("X")
+  strikes.onNext("X")
 }
 
+example(of: "Filter") {
+  let disposeBag = DisposeBag()
+  Observable.of(1, 2, 3, 4, 5, 6)
+    .filter({ $0.isMultiple(of: 2) })
+    .subscribe(onNext: { print($0) })
+    .disposed(by: disposeBag)
+}
