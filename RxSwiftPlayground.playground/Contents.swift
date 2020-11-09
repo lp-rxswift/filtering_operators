5542a7797489ccd4f0a7c80de8447e55a3d7f516
Lucas <lucaspedrazoli@gmail.com>
2020-11-09 16:46:24 -0300

    add take example

--- RxSwiftPlayground.playground/Contents.swift
+++ RxSwiftPlayground.playground/Contents.swift
@@ -72,3 +72,13 @@ example(of: "Skip until") {
   trigger.onNext("X")
   subject.onNext("C")
 }
+
+example(of: "Take") {
+  let disposeBag = DisposeBag()
+  Observable.of(1, 2, 3, 4, 5, 6)
+    .take(3)
+    .subscribe(onNext: { print($0) })
+    .disposed(by: disposeBag)
+}
+
+
