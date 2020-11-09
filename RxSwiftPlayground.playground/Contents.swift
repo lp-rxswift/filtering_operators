047a1a0c9b5bc271def0f5f5ed538cd8f841b2a3
Lucas <lucaspedrazoli@gmail.com>
2020-11-09 17:04:02 -0300

    add take until trigger example

--- RxSwiftPlayground.playground/Contents.swift
+++ RxSwiftPlayground.playground/Contents.swift
@@ -106,3 +106,19 @@ example(of: "take until") {
     .subscribe(onNext: { print($0) })
     .disposed(by: disposeBag)
 }
+
+example(of: "take until trigger") {
+  let disposeBag = DisposeBag()
+  let subject = PublishSubject<String>()
+  let trigger = PublishSubject<String>()
+
+  subject
+    .takeUntil(trigger)
+    .subscribe(onNext:{ print($0) })
+    .disposed(by: disposeBag)
+
+  subject.onNext("1")
+  subject.onNext("2")
+  trigger.onNext("X")
+  subject.onNext("3")
+}
